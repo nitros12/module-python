d7ce26da14eb3821009a3da39d9fa83bce2bace9
Nevexo <nevexo@outlook.com>
2017-08-16 11:50:37 +0100

    Update analyticord.py

--- analyticord.py
+++ analyticord.py
@@ -19,7 +19,7 @@ def init(tokenB):
         if noMessages == False: print("[AC] Logged in to Analyticord as " + data['name'])
         
     if checkForUpdates:
-        r = requests.get(server + "/api/version?lib=python",  headers={'Authorization': 'bot srOQXHXBW2TSMzhj2OMtuL3gXZiSppvZm7CIRImUjGAm6Q2K91Z5XrtMF5YEXawT0zQzVggmcvLFUmYzleRkymNStJWVHPvelKCArEZA4cIwWDBXIkaaotxSYz3QUplR'})
+        r = requests.get(server + "/api/version?lib=python")
         body = r.text
         if float(body) > version:
             print("[AC] Version " + str(body) + " is avaliable, you're running " + str(version) + " update to use that latest features. https://github.com/analyticord/module-python")
@@ -31,7 +31,7 @@ def send(eventType, data):
         print("[AC] Failed to send data, " + eventType + "/" + data)
     else:
         if verifiedMessages:
-            print("[AC] Success! The data was sent! Verify that it's the correct information at: https://analyticord.solutions/api/verified?id=" + body['ID'])
+            print("[AC] Success! The data was sent! Verify that it's the correct information at: https://analyticord.solutions/api/verified?id=" + body['ID'] + " or type ]verify " + body['ID'] + " in our Discord Server.")
 
 def message():
     global messages
@@ -42,13 +42,6 @@ def submitMessages():
     global messages
     threading.Timer(60.0, submitMessages).start() 
     if messages > 0:
-        r = requests.post(server + "/api/submit", data={'eventType': "messages", 'data': messages},  headers={'Authorization': 'bot ' + token})
-        body = r.json()
-        if body.get('error'):
-            print("[AC] Something went wrong while trying to send data.")
-        else:
-            messages = 0
-            if verifiedMessages:
-                print("[AC] Success! The data was sent! Verify that it's the correct information at: https://analyticord.solutions/api/verified?id=" + body['ID'])
+        send('messages', messages)
                 
 submitMessages()
