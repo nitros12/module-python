d178a7cf02774fb62011d17d896fd3d94fc492e7
ben simms <ben@bensimms.moe>
2017-08-16 22:12:44 +0100

    cleanup and add getData endpoint

--- analyticord/analyticord.py
+++ analyticord/analyticord.py
@@ -1,14 +1,19 @@
 import asyncio
 import aiohttp
+import logging
+
+logger = logging.getLogger("analyticord")
 
 
 class ApiError(Exception):
-    def __init__(self, code: int, desc: str):
-        self.code = code
-        self.desc = desc
+    def __init__(self, error: str, description: str, status: int, **extra):
+        self.code = error
+        self.desc = description
+        self.status = status
+        self.extra = extra
 
     def __str__(self):
-        return f"API Error id {self.code}: {self.desc}"
+        return f"API Error id {self.code}: {self.desc} (http code: {self.status}). Extra attrs: {self.extra}"
 
 
 class AnalytiCord:
@@ -16,6 +21,7 @@ class AnalytiCord:
     base_address = "https://analyticord.solutions"
     login_address = base_address + "/api/botLogin"
     send_address = base_address + "/api/submit"
+    get_address = base_address + "/api/getData"
 
     def __init__(self,
                  token: str,
@@ -34,6 +40,10 @@ class AnalytiCord:
 
         self.loop.create_task(self.start())
 
+    @property
+    def auth(self):
+        return {"Authorization": self.token}
+
     async def start(self):
         """Start up analyticord connection.
         Also runs the message updater loop
@@ -42,12 +52,10 @@ class AnalytiCord:
         """
         async with self.session.get(
                 AnalytiCord.login_address,
-                headers=dict(Authorization=self.token)) as resp:
+                headers=self.auth) as resp:
             if resp.status != 200:
                 err = await resp.json()
-                code = int(err.get("error", -1))
-                desc = err.get("description")
-                raise ApiError(code=code, desc=desc)
+                raise ApiError(status=resp.status, **err)
 
         if self.do_message_loop:
             self.loop.create_task(self.update_messages_loop())
@@ -63,12 +71,23 @@ class AnalytiCord:
         async with self.session.post(
                 AnalytiCord.send_address,
                 data=dict(eventType=event_type, data=data),
-                headers=dict(Authorization=self.token)) as resp:
+                headers=self.auth) as resp:
             if resp.status != 200:
                 err = await resp.json()
-                code = int(err.get("error", -1))
-                desc = err.get("description")
-                raise ApiError(code=code, desc=desc)
+                raise ApiError(status=resp.status, **err)
+
+    async def get(self, **attrs) -> dict:
+        """Get data from the api.
+
+        :param attrs: Kwarg attributes passed to get request params.
+
+        :return: Tuple of response code and Dict response from api. (status, dict).
+        """
+        async with self.session.get(
+                AnalytiCord.get_address,
+                params=attrs,
+                headers=self.auth) as resp:
+            return (resp.status, await resp.json())
 
     async def increment_messages(self, *_):
         """Increment the message count.
@@ -78,13 +97,19 @@ class AnalytiCord:
         async with self.message_lock:
             self.message_count += 1
 
+    async def update_messages_now(self):
+        async with self.message_lock:
+            await self.send("messages", str(self.message_count))
+            self.message_count = 0
+
     async def update_messages_loop(self):
         while True:
             await asyncio.sleep(self.message_interval)
             if self.message_count:
-                async with self.message_lock:
-                    await self.send("messages", str(self.message_count))
-                    self.message_count = 0
+                try:
+                    await self.update_messages_now()
+                except ApiError as e:
+                    logger.error(str(e))
 
     def hook_bot(self, bot):
         """Hook the on_message events of a discord.py bot."""
